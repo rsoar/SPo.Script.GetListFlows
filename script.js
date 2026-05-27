a638b5e6d80aaaa207e2c979876a282a724dbbf2
Rafael Medeiros <77183668+rsoar@users.noreply.github.com>
2026-05-27 14:43:26 -0300

    Refactor RestRequest to async/await
    Refactor RestRequest to use async/await and improve logging.

--- script.js
+++ script.js
@@ -8,45 +8,39 @@ function RestRequest(url, params) {
       if (req.status >= 200 && req.status < 300) {
         resolve(req);
       } else {
-        reject({
-          status: req.status,
-          statusText: req.statusText,
-        });
+        reject({ status: req.status, statusText: req.statusText });
       }
     };
-
     var webBasedUrl = (
-      window._spPageContextInfo.webServerRelativeUrl +
-      "//" +
-      url
+      window._spPageContextInfo.webServerRelativeUrl + "//" + url
     ).replace(/\/{2,}/, "/");
     req.open("POST", webBasedUrl, true);
     req.setRequestHeader("Content-Type", "application/json;odata=verbose");
     req.setRequestHeader("ACCEPT", "application/json;odata=verbose");
-    req.setRequestHeader(
-      "x-requestdigest",
-      window._spPageContextInfo.formDigestValue
-    );
+    req.setRequestHeader("x-requestdigest", window._spPageContextInfo.formDigestValue);
     req.send(params ? JSON.stringify(params) : void 0);
   });
 }
 
-RestRequest(
-  `/_api/web/GetList(@a1)/SyncFlowInstances?@a1='${listURL}'`,
-  null
-).then(function (resp) {
-  var output = JSON.parse(resp.response);
-  var flowinstances = JSON.parse(output.d.SynchronizationData).value;
-  for (var i = 0; i < flowinstances.length; i++) {
-    console.log(
-      "Name: " +
-        flowinstances[i].name +
-        "\r\nDisplayName: " +
-        flowinstances[i].properties.displayName +
-        "\r\nEnvironment Name: " +
-        flowinstances[i].properties.environment.name +
-        "\r\nModified: " +
-        flowinstances[i].properties.lastModifiedTime
-    );
+(async () => {
+  try {
+    const resp = await RestRequest(`/_api/web/GetList(@a1)/SyncFlowInstances?@a1='${listURL}'`, null);
+    const output = JSON.parse(resp.response);
+    const flowinstances = JSON.parse(output.d.SynchronizationData).value;
+
+    if (flowinstances.length === 0) {
+      console.log("Nenhum fluxo encontrado para essa lista.");
+      return;
+    }
+
+    console.log(`Total de fluxos encontrados: ${flowinstances.length}\n`);
+    console.table(flowinstances.map(f => ({
+      DisplayName: f.properties.displayName,
+      Modified: f.properties.lastModifiedTime,
+      URL: `https://make.powerautomate.com/environments/${f.properties.environment.name}/flows/${f.name}`
+    })));
+
+  } catch (err) {
+    console.error("Erro na requisição:", err);
   }
-});
+})();
